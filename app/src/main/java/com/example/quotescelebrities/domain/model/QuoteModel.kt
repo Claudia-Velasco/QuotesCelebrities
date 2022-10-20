c35a32557afc18204228b69c69b1e15a69deb6cb
Claudia-Velasco <29lupitha@gmail.com>
2022-10-20 08:55:36 -0500

    implementation pattern repository

--- app/src/main/java/com/example/quotescelebrities/domain/model/QuoteModel.kt
+++ app/src/main/java/com/example/quotescelebrities/domain/model/QuoteModel.kt
@@ -1,3 +1,3 @@
 package com.example.quotescelebrities.domain.model
 
-data class QuoteModel(val quote: String, val author: String)
+data class QuoteModel(val id:Int, val quote: String, val author: String)
